8239fed885cd9f962f4c8538d81b8f1f56e0af0d
taka- <takayuki.for.public@gmail.com>
2013-02-24 16:22:53 +0900

    added ex2-3 1

--- 2-3.py
+++ 2-3.py
@@ -1,9 +1,16 @@
 import Pmf
+from operator import itemgetter
 
 def Mode(hist):
     " Return the most frquency value from Hist Object"
     return reduce(lambda sum, x: sum if sum[1] > x[1] else x, iter(hist.Items()))
 
-hist = Pmf.MakeHistFromList([1, 2, 2, 3, 5])
+def AllModes(hist):
+    " Return all values with sorted order by desc"
+    getcount = itemgetter(1)
+    return sorted(hist.Items(), key=getcount)
+
+hist = Pmf.MakeHistFromList([1,2, 2, 3, 3, 3, 5])
 print Mode(hist)
+print AllModes(hist)
 
